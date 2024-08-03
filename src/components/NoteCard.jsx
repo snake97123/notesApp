54a81eed225e53cdfbb11a8069326a9c059b145a
snake97123 <73522198+snake97123@users.noreply.github.com>
2024-08-03 13:22:43 +0900

    ドラッグアンドドロップ機能の実装

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -1,12 +1,14 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState, useCallback } from "react";
 import Trash from "../icons/Trash";
 
 const NoteCard = ({ note }) => {
   const body = JSON.parse(note.body);
-  const position = JSON.parse(note.position);
+  const [position, setPosition] = useState(JSON.parse(note.position));
   const colors = JSON.parse(note.colors);
 
-  // const [textAreaHeight, setTextAreaHeight] = useState("auto");
+  let mouseStartPosition = { x: 0, y: 0 };
+  const cardRef = useRef(null);
+
   const textAreaRef = useRef(null);
 
   useEffect(() => {
@@ -23,8 +25,35 @@ const NoteCard = ({ note }) => {
     textArea.style.height = `${textArea.scrollHeight}px`; // Set the new height
   };
 
+  const mouseMove = useCallback((event) => {
+    const dx = event.clientX - mouseStartPosition.x;
+    const dy = event.clientY - mouseStartPosition.y;
+
+    setPosition((prevPosition) => ({
+      x: prevPosition.x + dx,
+      y: prevPosition.y + dy,
+    }));
+
+    mouseStartPosition.x = event.clientX;
+    mouseStartPosition.y = event.clientY;
+  }, []);
+
+  const mouseDown = (event) => {
+    mouseStartPosition.x = event.clientX;
+    mouseStartPosition.y = event.clientY;
+
+    document.addEventListener("mousemove", mouseMove);
+    document.addEventListener("mouseup", mouseUp);
+  };
+
+  const mouseUp = () => {
+    document.removeEventListener("mousemove", mouseMove);
+    document.removeEventListener("mouseup", mouseUp);
+  };
+
   return (
     <div
+      ref={cardRef}
       className="card"
       style={{
         backgroundColor: colors.colorBody,
@@ -34,6 +63,7 @@ const NoteCard = ({ note }) => {
       }}
     >
       <div
+        onMouseDown={mouseDown}
         className="card-header"
         style={{
           backgroundColor: colors.colorHeader,
